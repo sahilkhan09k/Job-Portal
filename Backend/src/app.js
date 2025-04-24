2bf271c71c7261bea7248ffb2bd96cbbdae5f446
sahilkhan09k <sahilkhan392005@gmail.com>
2025-04-24 14:55:13 +0530

    Add cookie-parser and cors middleware; implement apiError and apiResponse utility classes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,31 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-export {app}
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export { app };
