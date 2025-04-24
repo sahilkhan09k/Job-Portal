65bd5e8760b5684323db1e23377cb8b66554c8f8
sahilkhan09k <sahilkhan392005@gmail.com>
2025-04-24 14:39:50 +0530

    Database connected

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import { app } from './app.js';
+import { connectDB } from './db/index.js';
+
+dotenv.config({
+    path : "./.env"
+})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error) => {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+});
+
